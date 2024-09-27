8014d74272982e360c6476585f37731997ba9a1d
mohammad khakshoor <mkhbeats@gmail.com>
2024-09-27 14:52:36 +0330

    feat: add lenis package for smooth scrolling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Inter } from "next/font/google";
 import localFont from "next/font/local";
+import { ReactLenis } from "@/helpers/lenisScroll";
 
 // Font files can be colocated inside of `app`
 const whiteList = localFont({
@@ -22,7 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${whiteList.variable}  antialiased`}>{children}</body>
+      <ReactLenis root>
+        <body className={`${inter.className} ${whiteList.variable}  antialiased`}>{children}</body>
+      </ReactLenis>
     </html>
   );
 }
