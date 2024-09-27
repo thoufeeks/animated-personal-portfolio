1ae9c8984b636b5a154cff4478aea5d61eeca689
mohammad khakshoor <mkhbeats@gmail.com>
2024-09-27 14:54:18 +0330

    feat: add svg circle to navbar and fix minor issues like z-index

--- src/components/nabvar.js
+++ src/components/nabvar.js
@@ -1,15 +1,19 @@
+"use client";
 import React, { useReducer } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import NavbarIcon from "./svgs/navbar-icon";
 import { cn } from "@/helpers/cn";
 import ArrowRight from "./svgs/arrow-right";
+import Circle from "./svgs/circle";
 const links = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   { name: "Contact", href: "/contact" },
-  { name: "Blog", href: "/blog" },
-  { name: "Portfolio", href: "/portfolio" },
-  { name: "Services", href: "/services" },
+  { name: "Twitter", href: "https://twitter.com/mkh1999" },
+  { name: "Linkedin", href: "https://www.linkedin.com/in/mkh1999/" },
+  { name: "Github", href: "https://github.com/mkh1999" },
+  { name: "Instagram", href: "https://www.instagram.com/mkh1999/" },
+  { name: "Youtube", href: "https://www.youtube.com/channel/UC1x0-4-8w7z-1-2-3-4" },
 ];
 const variants = {
   closed: {
@@ -94,34 +98,45 @@ const Navbar = () => {
       variants={variants}
       initial="closed"
       animate={isNavbarOpen ? "open" : "closed"}
-      className="fixed flex-col p-1.5 top-2 bg-gray-950 flex justify-center items-center bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20"
+      className=" flex-col z-[888888888]
+        p-1.5 top-2 bg-gray-950 flex justify-center items-center bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20"
     >
-      <motion.div className={cn("p-1.5 bg-gray-900 flex flex-col")} variants={navVariants}>
+      <motion.div className={cn("p-1.5 relative bg-gray-900 flex overflow-hidden flex-col")} variants={navVariants}>
+        <div
+          className={cn(
+            "absolute transition-all duration-1000 delay-150 inset-0 translate-y-10 translate-x-32 pointer-events-none",
+            isNavbarOpen ? "opacity-100" : "opacity-0"
+          )}
+        >
+          <Circle className="stroke-gray-800 scale-125" />
+        </div>
         <div className="flex gap-7 sm:gap-16 w-full  items-center justify-between">
           <p className="leading-none bg-gray-700  p-0.5 size-9 grid place-content-center rounded-full text-gray-50">
             MK
           </p>
-          <p className="leading-none bg-gradient-to-r from-yellow-500 to-pink-500 text-transparent bg-clip-text py-2  px-1 sm:p-0.5  whitespace-nowrap font-semibold relative top-1 text-lg sm:text-2xl font-myfont">
+          <p className="leading-none bg-gradient-to-r from-yellow-500 to-pink-500 text-transparent bg-clip-text py-2  px-1 sm:p-0.5  whitespace-nowrap font-semibold relative top-1 text-lg sm:text-2xl font-whiteList">
             Front-end Dev
           </p>
           <span className={cn("size-9 bg-gray-700  p-0.5", isNavbarOpen ? "rounded-full" : "rounded-full")}>
             <NavbarIcon isNavbarOpen={isNavbarOpen} toggleNavbar={toggleNavbar} />
           </span>
         </div>
-        <motion.ul variants={ulVariants} className="self-start">
-          {links.map((link) => (
-            <motion.li
-              className="leading-none flex items-center gap-3 py-2 pl-1 text-xl text-gray-50 font-myfont"
-              variants={liVariants}
-              key={link}
-            >
-              <span className="bg-gray-700 border-2 border-dashed border-gray-600 p-0.5 size-7 font-inter grid place-content-center rounded-full">
-                <ArrowRight stroke="white" isNavbarOpen={isNavbarOpen} />
-              </span>
-              <span className="relative top-1">{link.name}</span>
-            </motion.li>
-          ))}
-        </motion.ul>
+        <AnimatePresence>
+          <motion.ul variants={ulVariants} className="self-start">
+            {links.map((link) => (
+              <motion.li
+                className="leading-none flex items-center gap-3 py-2 pl-1 text-xl text-gray-50 font-SFProDisplay"
+                variants={liVariants}
+                key={link}
+              >
+                <span className="bg-gray-700 border-2 border-dashed border-gray-600 p-0.5 size-7 font-inter grid place-content-center rounded-full">
+                  <ArrowRight stroke="white" isNavbarOpen={isNavbarOpen} />
+                </span>
+                <span className="">{link.name}</span>
+              </motion.li>
+            ))}
+          </motion.ul>
+        </AnimatePresence>
       </motion.div>
     </motion.nav>
   );
---
+++ src/components/svgs/circle.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Circle = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" className="size-full">
+      <g {...props} fill="none">
+        <circle cx="100" cy="100" r="95" />
+        <circle cx="100" cy="100" r="90" />
+        <circle cx="100" cy="100" r="85" />
+        <circle cx="100" cy="100" r="80" />
+        <circle cx="100" cy="100" r="75" />
+        <circle cx="100" cy="100" r="70" />
+        <circle cx="100" cy="100" r="65" />
+        <circle cx="100" cy="100" r="60" />
+        <circle cx="100" cy="100" r="55" />
+        <circle cx="100" cy="100" r="50" />
+        <circle cx="100" cy="100" r="45" />
+        <circle cx="100" cy="100" r="40" />
+        <circle cx="100" cy="100" r="35" />
+        <circle cx="100" cy="100" r="30" />
+        <circle cx="100" cy="100" r="25" />
+        <circle cx="100" cy="100" r="20" />
+        <circle cx="100" cy="100" r="15" />
+        <circle cx="100" cy="100" r="10" />
+        <circle cx="100" cy="100" r="5" />
+      </g>
+    </svg>
+  );
+};
+
+export default Circle;
